134712d9315b689599c2a13f747692af9fb7feba
Tien Pham <pvtienlamk@gmail.com>
2024-06-25 23:46:18 +0700

    The first commit

---
+++ src/main/java/com/example/WorkFlowManagement/WorkFlowManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.WorkFlowManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFlowManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkFlowManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WorkFlowManagement/WorkFlowManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorkFlowManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFlowManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
